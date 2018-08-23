d2c7f9b0bcd2369fce901dc5141d3cc20de5843f
18189867377 <you@example.com>
2018-08-23 17:28:17 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/eurekeserver/EurekeServerApplication.java
@@ -0,0 +1,14 @@
+package com.roncoo.eshop.eurekeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekeServerApplication.class, args);
+    }
+}
